6a79e7bca5fcd848b923e5b0e8cdf350b7ea3713
Mª Teresa (Mayte) Fernández Coro <79691310+teresafcoro@users.noreply.github.com>
2023-11-16 10:32:31 +0100

    lab07

---
+++ lab07/linq/Functions.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace TPP.Laboratory.Functional.Lab07
+{
+
+    static public class Functions
+    {
+
+        public static IEnumerable<TResult> Map<TElement, TResult>(this IEnumerable<TElement> collection, Func<TElement, TResult> function) {
+            /* Implementación previa:
+             * 
+                TResult[] result = new TResult[collection.Count()];
+                uint i = 0;
+                foreach (TElement d in collection)
+                {
+                    result[i] = function(d);
+                    i++;
+                }
+                return result;
+            */
+
+            // Nueva implementación -> Usando generadores:
+            foreach (TElement d in collection)
+            {
+                yield return function(d);
+            }
+        }
+
+        /* public static void ForEach<TElement>(this IEnumerable<TElement> collection, Action<TElement> action)
+        {
+            foreach (TElement d in collection)
+            {
+                action(d);
+            }            
+        } */
+
+        public static IEnumerable<TElement> ForEach<TElement>(this IEnumerable<TElement> collection, Action<TElement> action)
+        {
+            foreach (TElement d in collection)
+            {
+                action(d);
+                yield return d;
+            }            
+        }
+
+        public static IEnumerable<TElement> Filter<TElement>(this IEnumerable<TElement> collection, Predicate<TElement> function)
+        {
+            foreach (TElement d in collection)
+            {
+                if (function(d))
+                    yield return d;
+            }
+        }
+
+        public static double MyAverage(this IEnumerable<int> collection)
+        {
+            // int count = collection.Count();
+            int count = 0; // Haciendo manualmente el count se evita consumir dos veces la colección
+            double total = 0;
+            foreach (var x in collection)
+            {
+                total += x;
+                count++;
+            }
+            return total / count;
+        }
+
+        public static IEnumerable<int> MyRandom()
+        {
+            var random = new Random();
+            while (true)
+            {
+                yield return random.Next(1, 5);
+            }
+        }
+    }
+
+}
---
+++ lab07/linq/Program.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using System;
+using System.Linq;
+
+namespace TPP.Laboratory.Functional.Lab07 {
+
+    class Program {
+
+        static void Main() {
+            IEnumerable<int> integers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            // integers.Map(x => x + 1); // .Map(x => seguido de una expresión ; o
+            
+            var x = integers.Map(x =>
+            {
+                Console.Write($"{x} ");
+                return x;
+            }).First(); // seguido de un bloque
+            Console.WriteLine($"\n{x}");
+
+            /* -> Al invocarlo no muestra lo que se espera (no muestra nada) por consola.
+             * Se debe forzar que se ejecute, activar el generador.
+             * Añado entonces la invocación al método Last() de System.Linq.
+             * También existe el método First() que devuelve el primero y mata al generador,
+             * no continúa calculando.
+             */
+
+            // Como estamos recorriendo y mostrando mejor usar ForEach (no es un generador):
+            /* integers.ForEach(x =>
+            {
+                Console.Write($"{x} ");
+            }); */
+            // integers.ForEach(Console.WriteLine);
+
+            // Nueva impl de ForEach (sí es un generador):
+            integers.ForEach(x =>
+            {
+                Console.Write($"x1: {x} ");
+            }).Map(x => x+1).ForEach(x =>
+            {
+                Console.Write($"x2: {x} ");
+            }).Last(); // Acordarse de consumir el generador!
+            // Va consumiendo de uno en uno los valores de la colección,
+            // pasando por todos los generadores (pipeline)
+
+            Console.WriteLine();
+
+            // Ejemplo:
+            var avg = integers.ForEach(x =>
+            {
+                Console.Write($"{x} ");
+            }).MyAverage();
+            Console.Write($"\navg: {avg} ");
+            // Se está consumiendo dos veces el generador...
+            // ¿dónde? en ForEach al consumir la colección varias veces (count + for)
+
+            // Otro ejemplo por si se ve mejor...
+            avg = Functions.MyRandom().Take(2).ForEach(x =>
+            {
+                Console.Write($"{x} ");
+            }).MyAverage();
+            Console.Write($"\navg: {avg} ");
+        }
+    }
+}
