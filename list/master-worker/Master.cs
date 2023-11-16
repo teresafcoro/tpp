49eeba974d003b2e62abfa6dbb2f768e71d69ea6
Mª Teresa (Mayte) Fernández Coro <79691310+teresafcoro@users.noreply.github.com>
2023-11-16 11:36:47 +0100

    list - part4

---
+++ list/master-worker/Master.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+using list;
+
+namespace master
+{
+    public class Master<T>
+    {
+        private ConcurrentQueue<T> queue;
+        private uint numberOfThreads;
+
+        public Master(ConcurrentQueue<T> queue, uint numberOfThreads)
+        {
+            if (numberOfThreads < 1 || numberOfThreads > queue.NumberOfElements())
+                throw new ArgumentException("The number of threads must be lower or equal to the number of elements in the queue.");
+            this.queue = queue;
+            this.numberOfThreads = numberOfThreads;
+        }
+
+        public double ComputeModulus()
+        {
+            Worker<T>[] workers = new Worker<T>[numberOfThreads];
+            int elementsPerThread = (int)(queue.NumberOfElements() / numberOfThreads);
+            for (int i = 0; i < this.numberOfThreads; i++)
+                workers[i] = new Worker<T>(queue,
+                    i * elementsPerThread,
+                    (i < numberOfThreads - 1) ? (i + 1) * elementsPerThread - 1 : (int)(queue.NumberOfElements() - 1) // last one
+                    );
+            Thread[] threads = new Thread[workers.Length];
+            for (int i = 0; i < workers.Length; i++)
+            {
+                threads[i] = new Thread(workers[i].Compute);
+                threads[i].Name = "Worker Vector Modulus " + (i + 1);
+                threads[i].Priority = ThreadPriority.BelowNormal;
+                threads[i].Start();
+            }
+            foreach (Thread thread in threads)
+                thread.Join();
+            long result = 0;
+            foreach (Worker<T> worker in workers)
+                result += worker.Result;
+            return result;
+        }
+    }
+}
---
+++ list/master-worker/Worker.cs
@@ -0,0 +1,26 @@
+﻿using list;
+
+namespace master
+{
+    internal class Worker<T>
+    {
+        private int fromIndex, toIndex;
+        private ConcurrentQueue<T> queue;
+
+        internal long Result { get; set; }
+
+        internal Worker(ConcurrentQueue<T> queue, int fromIndex, int toIndex)
+        {
+            this.queue = queue;
+            this.fromIndex = fromIndex;
+            this.toIndex = toIndex;
+        }
+
+        internal void Compute()
+        {
+            Result = 0;
+            for (int i=this.fromIndex; i<=this.toIndex; i++)
+                Result += i;
+        }
+    }
+}
